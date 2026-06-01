14eeb425b664e002c0b0773cbc50efcf64316877
Thiveyabaskaran <122294169+Thiveyabaskaran@users.noreply.github.com>
2026-06-01 14:02:05 +0800

    DisplayInfoWITHPRINT
    Fix typo in print statements and add overloaded displayInfo methods.

--- Info.java
+++ Info.java
@@ -7,6 +7,12 @@ public class Info implements InterfaceInfo{
     }   
 
     public void displayInfo(){
-
+        System.Out.pritnln("Welcome To Math Quiz !");
+    }
+    public void displayInfo(int page){
+        System.Out.pritnln("First ODE" + page + "/10");
+    }
+    public void displayInfo(double score){
+        System.Out.pritnln("Score" + score + "%");
     }
 }
