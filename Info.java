88d2e35346885e583d9e414a0c743ce4e0f974f7
Thiveyabaskaran <122294169+Thiveyabaskaran@users.noreply.github.com>
2026-06-01 22:08:26 +0800

    Fix typos in displayInfo method outputs

--- Info.java
+++ Info.java
@@ -7,17 +7,17 @@ public class Info implements InterfaceInfo{
     }   
 
     public void displayInfo(){
-        System.Out.pritnln("Welcome To Math Quiz !");
+        System.Out.println("Welcome To Math Quiz !");
     }
     public void displayInfo(int page){
-        System.Out.pritnln("First ODE" + page + "/10");
+        System.Out.println("First ODE" + page + "/10");
     }
     public void displayInfo(double score){
-        System.Out.pritnln("Score" + score + "%");
-        if score (>=80) System.Out.pritnln("Spectacular !");
+        System.Out.println("Score" + score + "%");
+        if score (>=80) System.Out.println("Spectacular !");
         else if score (>=60) System.Out.pritnln("That's Good !");
-        else if score (>=40) System.Out.pritnln("Great Effort !");
-        else if score (>=20) System.Out.pritnln("Lets's elavate next time !");
+        else if score (>=40) System.Out.println("Great Effort !");
+        else if score (>=20) System.Out.println("Lets's elavate next time !");
         else System.Out.println("Don't Give Up !")
     }
 }
