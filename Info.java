7b5592302f6f868fada420cef0b582e128790b6e
Thiveyabaskaran <122294169+Thiveyabaskaran@users.noreply.github.com>
2026-06-01 14:12:02 +0800

    display logic
    Fix typos in print statements and update score conditions.

--- Info.java
+++ Info.java
@@ -14,5 +14,10 @@ public class Info implements InterfaceInfo{
     }
     public void displayInfo(double score){
         System.Out.pritnln("Score" + score + "%");
+        if score (>=80) System.Out.pritnln("Spectacular !");
+        else if score (>=60) System.Out.pritnln("That's Good !");
+        else if score (>=40) System.Out.pritnln("Great Effort !");
+        else if score (>=20) System.Out.pritnln("Lets's elavate next time !");
+        else System.Out.println("Don't Give Up !")
     }
 }
